2e48b781ce5066e1968089be576d7ee9383b72be
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-05-16 01:49:30 -0300

    feat: criação projeto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-zinc-950">
+      <Text className="text-2xl font-bold text-zinc-50">Tailwindcss</Text>
+      <Text className="text-2xl font-bold text-zinc-50">Spacetime</Text>
+      <StatusBar style="light" translucent />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
